95d24ab200ad3f72de4a5e824951f6708788d2c8
HeartLessSad <37054562+HeartLessSad@users.noreply.github.com>
2021-04-04 22:38:47 +0800

    Update wkzz.js

--- wkzz.js
+++ wkzz.js
@@ -60,7 +60,7 @@ hostname = wx.tiantianaiyuedu.site
 
 
 const $ = new Env('微客众智自动阅读');
-let wkzz = require('./wkzh.json');
+const wkzz = require('./wkzh.json');
 let times = Math.round(Date.now() / 1000)
 let wkzzurl = '', wkzzhd = '',id = '',uid='',tid='',name=''
 !(async () => {
